397614920d894f7796771a464968a2e82db8cdab
miguelmusilva <miguel.mu.silva@gmail.com>
2022-11-16 20:29:24 +0100

    Prueba descarga

---
+++ scripts/prueba.sh
@@ -0,0 +1,36 @@
+
+#file="/home/vant/decont/data/urls"
+lines=$(cat $1)
+#downloadFolder="/home/vant/decont/data"
+
+echo $1
+echo $2
+echo $3
+
+for line in $lines
+do
+	echo $line
+	wget $line -P $2
+done
+
+
+
+if [[ $3 == *"yes"* ]]; then
+	gunzip -k $2/*.gz
+	
+	if [ ! -z "$4" ]; then
+    		echo "1st argument supplied"
+    		for file in $2/*.fastq
+    		do
+    			echo $file
+    			sed -i "/$4/{N;N;N;d}" $file
+    		done 
+	fi
+fi
+
+
+
+
+#-k: no elimina los .gz
+# N: 
+
