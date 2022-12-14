7d3de4e1b2ee505e62dbf1fd612cd0b5651b4e31
miguelmusilva <miguel.mu.silva@gmail.com>
2022-12-14 17:09:49 +0100

    Prueba Union Secuencias Fastqz

---
+++ scripts/prueba4.sh
@@ -0,0 +1,28 @@
+#$1 = 
+#$2 = 
+#$3 = 
+
+
+
+mkdir -p out/merged
+
+
+
+
+#echo directorio que contiene las secuencias
+#read varname5
+
+#echo directorio saliente
+#read varname6
+
+#echo sample ID
+#read varname7
+
+
+
+
+cat $1/$3*.fastq.gz > $2/$3.fastq.gz
+
+
+
+#file1.tar.gz file2.tar.gz > merged.tar.gz
