eebf89e539b7a0e7ce5e119cf8ff625bcb676580
miguelmusilva <miguel.mu.silva@gmail.com>
2022-12-18 13:58:02 +0100

    Final Index

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,8 +1,23 @@
-# This script should index the genome file specified in the first argument ($1),
-# creating the index in a directory specified by the second argument ($2).
 
-# The STAR command is provided for you. You should replace the parts surrounded
-# by "<>" and uncomment it.
+### INDEX SCRIPT ###
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+# $1 : file raw contaminants sequence
+# $2 : output directory
+
+
+mkdir -p res/contaminants_idx
+
+if [[ ! -e $2 ]] # Checking if already exists
+then
+	STAR    --runThreadN 4 \
+		--runMode genomeGenerate \
+		--genomeDir $2 \
+		--genomeFastaFiles $1 \
+		--genomeSAindexNbases 9
+else
+	echo "··········Index already exists. The script will continue··········"
+fi
+
+
+
+
