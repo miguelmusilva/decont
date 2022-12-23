f8f5db548cdda73545bd99f231933e480041b07d
miguelmusilva <miguel.mu.silva@gmail.com>
2022-12-23 20:02:54 +0100

    Final Version Download Script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,18 +1,34 @@
-# This script should download the file specified in the first argument ($1),
-# place it in the directory specified in the second argument ($2),
-# and *optionally*:
-# - uncompress the downloaded file with gunzip if the third
-#   argument ($3) contains the word "yes"
-# - filter the sequences based on a word contained in their header lines:
-#   sequences containing the specified word in their header should be **excluded**
-#
-# Example of the desired filtering:
-#
-#   > this is my sequence
-#   CACTATGGGAGGACATTATAC
-#   > this is my second sequence
-#   CACTATGGGAGGGAGAGGAGA
-#   > this is another sequence
-#   CCAGGATTTACAGACTTTAAA
-#
-#   If $4 == "another" only the **first two sequence** should be output
+
+### DOWNLOAD SCRIPT ###
+
+wget -N -i $1 -P $2 # Download and overwrite all files only if they are newer
+
+lines=$(cat $1) # md5 check to the download files
+for line in $lines
+do
+	online_md5= curl -sL $line.md5 | md5sum | cut -d ' ' -f 1
+	local_md5= md5sum $2/$(basename $line)
+	if [[ $online_md5 == $local_md5 ]]
+	then
+		echo -e "\nHurray, they are equal. The script will continue\n"
+	else
+		echo -e "\n:(. Sorry. This is quite problematic\n"
+		exit 0
+	fi	
+done
+
+if [[ $3 == "yes" ]] # Uncompressed the file only if third argument is yes
+then
+	if [[ ! -e $2/$(basename $1).fasta ]]
+	then
+		gunzip -k $2/*.gz
+	fi
+fi	
+
+if [ ! -z "$4" ] # Si existe algun argumento en $4, se eliminaran las coincidencias
+then
+	seqkit grep -v -r -p "$4" -n $2/$(basename $1).fasta > $2/$(basename $1)_filtered.fasta
+	echo -e "\n########## $(basename $1) filtered effortlessly ##########\n"
+fi
+
+echo -e "\nAll files are downloaded correctly, not corrupted and ready for use\n"
