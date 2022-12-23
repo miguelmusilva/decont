56068ad5fcc971b82cab412018919229eb0ccd0a
miguelmusilva <miguel.mu.silva@gmail.com>
2022-12-23 20:02:04 +0100

    Final Version Confirmation Script

--- scripts/confirmation.sh
+++ scripts/confirmation.sh
@@ -1,16 +1,25 @@
 
 ### CONFIRMATION SCRIPT ###
 
-echo "**********DECONTAMINATION OF SMALL-RNA SEQUENCING SAMPLES**********"
-echo "¿Are you sure you want to launch this script (y/n)?"
+programs=("fastqc" "seqtk" "cutadapt" "star" "multiqc") # Programs required for the entire script
+
+echo "********** DECONTAMINATION OF SMALL-RNA SEQUENCING SAMPLES **********"
+echo "The decontamination of RNA sequencing script is about to start. ¿Are you sure (y/n)?"
 read answer
 
 if [[ $answer = *[Nn]* ]] 
 then
-	echo "The script will not be launched"
-	exit
+	echo ·········· The script will not be launched ··········
+	exit 1
 elif [[ $answer = *[Yy]* ]]
 then
-	echo "The script will be launched. Take a coffee and relax"
+	echo ·········· The script will be launched. Take a coffee and relax ··········
+	for program in ${programs[@]} # Check the necessary programs list and download those non-existent 
+	do
+		which $program > /dev/null
+		if [ $? -eq 1 ]
+		then
+			mamba install -y $program
+		fi
+	done
 fi
-	
